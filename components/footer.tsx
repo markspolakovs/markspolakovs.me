91f06722f2da76494873d4126ceb5d4440365359
Marks Polakovs <marks@markspolakovs.me>
2022-09-01 22:39:19 +0100

    fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          All content copyright &copy; Marks Polakovs 2020&mdash;(date of his death + 70 years)
+          All content copyright &copy; Marks Polakovs 2020&mdash;(date of her/their death + 70 years)
           unless otherwise noted
         </div>
       </Container>
